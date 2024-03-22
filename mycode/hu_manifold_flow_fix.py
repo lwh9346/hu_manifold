39c044fb9c5e05d8094b97ce0d459a160966c641
lwh9346 <lwh9346@163.com>
2024-03-22 18:11:29 +0800

    initial commit

---
+++ mycode/hu_manifold_flow_fix.py
@@ -0,0 +1,193 @@
+import os
+import warnings
+import torch
+from torch.utils.data import DataLoader, Dataset
+from sympy import Symbol, Eq, Abs, tanh, Or, And
+import numpy as np
+
+import modulus.sym
+from modulus.sym.hydra import to_absolute_path, instantiate_arch, ModulusConfig
+from modulus.sym.utils.io import csv_to_dict
+from modulus.sym.solver import Solver
+from modulus.sym.domain import Domain
+from modulus.sym.geometry.primitives_3d import Box, Channel, Plane
+from modulus.sym.domain.constraint import (
+    PointwiseBoundaryConstraint,
+    PointwiseInteriorConstraint,
+    IntegralBoundaryConstraint,
+)
+from modulus.sym.domain.validator import PointwiseValidator
+from modulus.sym.domain.monitor import PointwiseMonitor
+from modulus.sym.key import Key
+from modulus.sym.node import Node
+from modulus.sym.eq.pdes.navier_stokes import NavierStokes
+from modulus.sym.eq.pdes.basic import NormalDotVec
+from modulus.sym.models.fourier_net import FourierNetArch
+from modulus.sym import ureg
+
+from hu_manifold_geometry import *
+
+
+@modulus.sym.main(config_path="conf", config_name="conf_flow_fix")
+def run(cfg: ModulusConfig) -> None:
+    # 定义方程与特征量
+    length_scale = quantity(cfg.custom.nd.length.value, cfg.custom.nd.length.unit)
+    velocity_scale = quantity(cfg.custom.nd.velocity.value, cfg.custom.nd.velocity.unit)
+    pressure_scale = quantity(cfg.custom.nd.pressure.value, cfg.custom.nd.pressure.unit)
+    nu = quantity(cfg.custom.ns.nu.value, cfg.custom.ns.nu.unit)
+    rho = quantity(cfg.custom.ns.rho.value, cfg.custom.ns.rho.unit)
+    nd = NonDimensionalizer(
+        length_scale=length_scale,
+        time_scale=length_scale / velocity_scale,
+        mass_scale=pressure_scale / velocity_scale**2 * length_scale**3,
+    )
+    ns = NavierStokes(nu=nd.ndim(nu), rho=nd.ndim(rho), dim=3, time=False)
+    ns_nodes = ns.make_nodes()
+    normal_dot_vel = NormalDotVec()
+    # 网络架构
+    input_keys = [Key("x"), Key("y"), Key("z")]
+    output_keys = [Key("u"), Key("v"), Key("w"), Key("p")]
+    flow_net = FourierNetArch(
+        input_keys, output_keys, frequencies_params=[i * 3 for i in range(11)]
+    )
+    flow_nodes = (
+        ns_nodes
+        + normal_dot_vel.make_nodes()
+        + [flow_net.make_node(name="flow_network")]
+    )
+    geo = SMM()
+    # 边界条件
+    flow_domain = Domain()
+    inlet_vol_flow = quantity(
+        cfg.custom.bc.inlet_vol_flow.value, cfg.custom.bc.inlet_vol_flow.unit
+    )
+    inlet_continuity = IntegralBoundaryConstraint(  # 入口流量
+        nodes=flow_nodes,
+        geometry=geo.inlet,
+        outvar={"normal_dot_vel": nd.ndim(inlet_vol_flow)},
+        batch_size=5,
+        integral_batch_size=cfg.batch_size.Inlet,
+        parameterization=geo.pr,
+        fixed_dataset=False,
+        batch_per_epoch=cfg.custom.batch_per_epoch,
+    )
+    flow_domain.add_constraint(inlet_continuity, "inlet_continuity")
+    outlet_pressure = PointwiseBoundaryConstraint(
+        nodes=flow_nodes,
+        geometry=geo.outlet,
+        outvar={"p": 0},
+        batch_size=cfg.batch_size.Outlet,
+        parameterization=geo.pr,
+        batch_per_epoch=cfg.custom.batch_per_epoch,
+    )
+    flow_domain.add_constraint(outlet_pressure, "outlet_pressure")
+    no_slip = PointwiseBoundaryConstraint(
+        nodes=flow_nodes,
+        geometry=geo.fluid,
+        outvar={"u": 0, "v": 0, "w": 0},
+        batch_size=cfg.batch_size.NoSlip,
+        parameterization=geo.pr,
+        criteria=Not(
+            Or(
+                geo.on_boundary_inlet,
+                geo.on_boundary_outlet,
+                geo.on_symmetry_xz,
+                geo.on_symmetry_yz,
+            )
+        ),
+        batch_per_epoch=cfg.custom.batch_per_epoch,
+    )
+    flow_domain.add_constraint(no_slip, "no_slip")
+    symmetry_xz = PointwiseBoundaryConstraint(
+        nodes=flow_nodes,
+        geometry=geo.fluid,
+        outvar={"v": 0},
+        batch_size=cfg.batch_size.Symmetry // 2,
+        parameterization=geo.pr,
+        criteria=geo.on_symmetry_xz,
+        batch_per_epoch=cfg.custom.batch_per_epoch,
+    )
+    flow_domain.add_constraint(symmetry_xz, "symmetry_xz")
+    symmetry_yz = PointwiseBoundaryConstraint(
+        nodes=flow_nodes,
+        geometry=geo.fluid,
+        outvar={"u": 0},
+        batch_size=cfg.batch_size.Symmetry // 2,
+        parameterization=geo.pr,
+        criteria=geo.on_symmetry_yz,
+        batch_per_epoch=cfg.custom.batch_per_epoch,
+    )
+    flow_domain.add_constraint(symmetry_yz, "symmetry_yz")
+    interior = PointwiseInteriorConstraint(
+        nodes=flow_nodes,
+        geometry=geo.fluid,
+        outvar={"continuity": 0, "momentum_x": 0, "momentum_z": 0, "momentum_y": 0},
+        batch_size=cfg.batch_size.Interior,
+        lambda_weighting={
+            "continuity": Symbol("sdf"),
+            "momentum_x": Symbol("sdf"),
+            "momentum_y": Symbol("sdf"),
+            "momentum_z": Symbol("sdf"),
+        },
+        compute_sdf_derivatives=True,
+        parameterization=geo.pr,
+        batch_per_epoch=cfg.custom.batch_per_epoch,
+    )
+    flow_domain.add_constraint(interior, "interior")
+    # 添加监视
+    inlet_sample = geo.inlet.sample_boundary(1000, parameterization=geo.pr)
+    outlet_sample = geo.outlet.sample_boundary(1000, parameterization=geo.pr)
+    flow_domain.add_monitor(
+        PointwiseMonitor(
+            inlet_sample,
+            ["p"],
+            metrics={
+                "inlet_pressure": lambda var: nd.dim(torch.mean(var["p"]), "kg/(m*s^2)")
+            },
+            nodes=flow_nodes,
+        )
+    )
+    flow_domain.add_monitor(
+        PointwiseMonitor(
+            outlet_sample,
+            ["p"],
+            metrics={
+                "outlet_pressure": lambda var: nd.dim(
+                    torch.mean(var["p"]), "kg/(m*s^2)"
+                )
+            },
+            nodes=flow_nodes,
+        )
+    )
+    flow_domain.add_monitor(
+        PointwiseMonitor(
+            inlet_sample,
+            ["u", "W_mi"],
+            metrics={
+                "inlet_u": lambda var: nd.dim(torch.mean(var["u"]), "m/s"),
+                "inlet_vol_flow": lambda var: ureg.convert(
+                    nd.dim(torch.mean(var["u"]) * torch.mean(var["W_mi"]), "m/s")
+                    * nd.dim(manifold_inlet_height, "m")
+                    * nd.dim(1, "m"),
+                    "m^3/s",
+                    "ml/s",
+                ),
+            },
+            nodes=flow_nodes,
+        )
+    )
+    flow_domain.add_monitor(
+        PointwiseMonitor(
+            outlet_sample,
+            ["w"],
+            metrics={"outlet_w": lambda var: nd.dim(torch.mean(var["w"]), "m/s")},
+            nodes=flow_nodes,
+        )
+    )
+    # 求解
+    flow_solver = Solver(cfg, flow_domain)
+    flow_solver.solve()
+
+
+if __name__ == "__main__":
+    run()
---
+++ mycode/hu_manifold_geometry.py
@@ -0,0 +1,229 @@
+from sympy import Symbol, Eq, tanh, Or, And, Not, Gt, Lt, Abs
+
+import numpy as np
+
+from modulus.sym.geometry import Parameterization
+from modulus.sym.geometry.primitives_3d import Box, Plane
+from modulus.sym import quantity
+from modulus.sym.eq.non_dim import NonDimensionalizer
+import time
+
+# 几何参数
+# 以下参数都是在1/4模型下的值，和文章中有出入
+
+# 几何尺寸
+# 均已无量纲化
+_nd = NonDimensionalizer(length_scale=quantity(1000, "um"))
+# 整个微流道
+channel_length = _nd.ndim(quantity(1500, "um"))  # 长度
+SMM_width = _nd.ndim(quantity(300, "um"))  # 宽度
+total_height = _nd.ndim(quantity(800, "um"))  # 高度
+# 微流道部分
+microchannel_unit_width = _nd.ndim(quantity(50, "um"))  # 一个微流道单元，W_c+W_w
+microchannel_width = Symbol("W_c")  # 流体通过部分宽度，文中是没有参数化的，这边给加上
+microchannel_wall_width = microchannel_unit_width - microchannel_width  # 微流道壁面厚度
+microchannel_height = _nd.ndim(quantity(200, "um"))  # 微流道高度
+microchannel_bottom_thickness = _nd.ndim(quantity(100, "um"))  # 微流道底部厚度
+# 歧管部分
+manifold_inlet_width = Symbol("W_mi")  # 歧管入口
+manifold_inlet_height = _nd.ndim(quantity(300, "um"))
+manifold_wall_width = _nd.ndim(quantity(100, "um"))  # 出入口之间的隔板
+manifold_outlet_width = SMM_width - manifold_inlet_width - manifold_wall_width  # 出口
+manifold_plate_width = Symbol("W_mp")  # 射流板的宽度
+manifold_plate_height = _nd.ndim(quantity(200, "um"))
+manifold_height = _nd.ndim(quantity(500, "um"))  # 歧管总高
+# 参数范围
+param_ranges = {
+    microchannel_width: (_nd.ndim(quantity(10, "um")), _nd.ndim(quantity(30, "um"))),
+    manifold_inlet_width: (_nd.ndim(quantity(50, "um")), _nd.ndim(quantity(150, "um"))),
+    manifold_plate_width: _nd.ndim(quantity(0, "um")),
+}
+fixed_param_ranges = {
+    microchannel_width: _nd.ndim(quantity(25, "um")),
+    manifold_inlet_width: _nd.ndim(quantity(50, "um")),
+    manifold_plate_width: _nd.ndim(quantity(100, "um")),
+}
+eps = 1e-9
+
+
+# 定义几何
+class SMM(object):
+    def __init__(self, parameterized: bool = False):
+        # 参数范围
+        print(
+            time.strftime(
+                "[%H:%M:%S] - Start building geometry", time.localtime(time.time())
+            )
+        )
+        if parameterized:
+            pr = Parameterization(param_ranges)
+            self.pr = param_ranges
+        else:
+            pr = Parameterization(fixed_param_ranges)
+            self.pr = fixed_param_ranges
+        # 微流道部分
+        microchannel_fluid = [
+            Box(
+                (
+                    microchannel_wall_width / 2 + index * microchannel_unit_width,
+                    0,
+                    microchannel_bottom_thickness,
+                ),
+                (
+                    microchannel_wall_width / 2
+                    + microchannel_width
+                    + index * microchannel_unit_width,
+                    SMM_width,
+                    microchannel_bottom_thickness + microchannel_height,
+                ),
+                parameterization=pr,
+            )
+            for index in range(30)
+        ]
+        microchannel_fluid = sum(microchannel_fluid[1:], start=microchannel_fluid[0])
+        microchannel_solid = (
+            Box(
+                (0, 0, 0),
+                (
+                    channel_length,
+                    SMM_width,
+                    microchannel_height + microchannel_bottom_thickness,
+                ),
+                parameterization=pr,
+            )
+            - microchannel_fluid
+        )
+        # 歧管部分
+        manifold_origin = (0, 0, microchannel_bottom_thickness + microchannel_height)
+        manifold_solid = (
+            Box(  # 出入口隔板
+                (
+                    manifold_origin[0],
+                    manifold_origin[1]
+                    + SMM_width
+                    - manifold_inlet_width
+                    - manifold_wall_width,
+                    manifold_origin[2],
+                ),
+                (
+                    manifold_origin[0] + channel_length,
+                    manifold_origin[1] + SMM_width - manifold_inlet_width,
+                    manifold_origin[2] + manifold_height,
+                ),
+                parameterization=pr,
+            )
+            + Box(  # 入口顶盖
+                (
+                    manifold_origin[0],
+                    manifold_origin[1] + SMM_width - manifold_inlet_width,
+                    manifold_origin[2] + manifold_inlet_height,
+                ),
+                (
+                    manifold_origin[0] + channel_length,
+                    manifold_origin[1] + SMM_width,
+                    manifold_origin[2] + manifold_height,
+                ),
+                parameterization=pr,
+            )
+            + Box(  # 射流板
+                (
+                    manifold_origin[0],
+                    manifold_origin[1]
+                    + SMM_width
+                    - manifold_inlet_width
+                    - manifold_wall_width
+                    - manifold_plate_width,
+                    manifold_origin[2],
+                ),
+                (
+                    manifold_origin[0] + channel_length,
+                    manifold_origin[1] + manifold_outlet_width,
+                    manifold_origin[2] + manifold_plate_height,
+                ),
+                parameterization=pr,
+            )
+        )
+        manifold_fluid = (
+            Box(
+                manifold_origin,
+                (
+                    manifold_origin[0] + channel_length,
+                    manifold_origin[1] + SMM_width,
+                    manifold_origin[2] + manifold_height,
+                ),
+                parameterization=pr,
+            )
+            - manifold_solid
+        )
+        # 流体域与固体域
+        self.solid = manifold_solid + microchannel_solid
+        self.fluid = manifold_fluid + microchannel_fluid
+        # 出入口
+        self.inlet = Plane(
+            (
+                0,
+                SMM_width - manifold_inlet_width,
+                microchannel_height + microchannel_bottom_thickness,
+            ),
+            (
+                0,
+                SMM_width,
+                microchannel_height
+                + microchannel_bottom_thickness
+                + manifold_inlet_height,
+            ),
+            parameterization=pr,
+        )
+        outlet_rotate_point = (0, 0, total_height)
+        self.outlet = Plane(
+            (
+                outlet_rotate_point[0],
+                outlet_rotate_point[1],
+                outlet_rotate_point[2] - channel_length,
+            ),
+            (
+                outlet_rotate_point[0],
+                outlet_rotate_point[1] + manifold_outlet_width,
+                outlet_rotate_point[2],
+            ),
+            parameterization=pr,
+        ).rotate(np.pi / 2, "y", outlet_rotate_point)
+        # 边界条件
+        x, y, z = Symbol("x"), Symbol("y"), Symbol("z")
+        self.on_boundary_inlet = And(
+            Lt(Abs(x - 0), eps),
+            Gt(y, SMM_width - manifold_inlet_width),
+            Gt(z, microchannel_bottom_thickness + microchannel_height),
+            # Lt(y, SMM_width), 不可能
+            Lt(
+                z,
+                microchannel_bottom_thickness
+                + microchannel_height
+                + manifold_inlet_height,
+            ),
+        )
+        self.on_boundary_outlet = And(
+            Lt(Abs(z - total_height), eps),
+            Lt(y, manifold_outlet_width),
+        )
+        self.on_symmetry_xz = Or(Lt(Abs(y - 0), eps), Lt(Abs(y - SMM_width), eps))
+        self.on_symmetry_yz = Lt(Abs(x - channel_length), eps)
+        print(time.strftime("[%H:%M:%S] - Geometry built", time.localtime(time.time())))
+
+
+if __name__ == "__main__":
+    from modulus.sym.utils.io.vtk import var_to_polyvtk
+
+    smm = SMM()
+    sb = smm.solid.sample_boundary(100000)
+    var_to_polyvtk(sb, "visualize/solid_boundary")
+    si = smm.solid.sample_interior(100000)
+    var_to_polyvtk(si, "visualize/solid_interior")
+    fb = smm.fluid.sample_boundary(100000)
+    var_to_polyvtk(fb, "visualize/fluid_boundary")
+    fi = smm.fluid.sample_interior(100000)
+    var_to_polyvtk(fi, "visualize/fluid_interior")
+    inlet = smm.inlet.sample_boundary(10000)
+    var_to_polyvtk(inlet, "visualize/inlet")
+    outlet = smm.outlet.sample_boundary(10000)
+    var_to_polyvtk(outlet, "visualize/outlet")
